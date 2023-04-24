ac2b73cb6a4d617e39fc878120745c72c99101b7
gabriel.plique@gmail.com <gabriel.plique@gmail.com>
2023-04-24 15:29:52 +0200

    exigence3

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,10 +2,44 @@ package ilu2;
 
 public class Welcome {
 
+
 	public static String welcome(String input) {
-		 input = input.substring(0, 1).toUpperCase() + input.substring(1);
-		 StringBuilder str = new StringBuilder("Hello, ");
-		 str.append(input);
-		 return str.toString();
+		String reponse = null;
+		String reponse3= null;
+//		String[] part;
+		if(" ".equals(input)||input==null||"".equals(input)){
+			 StringBuilder str2 = new StringBuilder("Hello, my friend");
+			 reponse=str2.toString();
+			 return reponse;
+		}
+	//	part=input.split(",");
+//		if(!part[0].equals(input)) {
+//			StringBuilder str4 = new StringBuilder("Hello");
+//			for(int i=0;i<part.length;i++) {
+//			
+//			String part1=part[i].substring(0, 1).toUpperCase() + input.substring(1);
+//			str4.append(", ");
+//			str4.append(part1);
+//			}
+//			reponse=str4.toString();
+//		}
+		else{
+			if(!input.equals(input.toUpperCase())){
+				reponse = input.substring(0, 1).toUpperCase() + input.substring(1);
+				StringBuilder str = new StringBuilder("Hello, ");
+				str.append(reponse);
+				reponse= str.toString();
+			}
+			if(input.equals(input.toUpperCase())) {
+				reponse = input.substring(0, 1).toUpperCase() + input.substring(1);
+				StringBuilder str3 = new StringBuilder("Hello, ");
+				str3.append(reponse);
+				str3.append(" !");
+				reponse3= str3.toString();
+				reponse=reponse3.toUpperCase();
+			}
+			
+		} 
+		return reponse;
 	}
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -7,8 +7,25 @@ import org.junit.jupiter.api.Test;
 class WelcomeTest {
 
 	@Test
-	void test() {
+	void test1() {
 		assertEquals(Welcome.welcome("bob"), "Hello, Bob");
 	}
-
+	
+	@Test
+	void test2() {
+		String verif="Hello, my friend";
+		assertEquals(Welcome.welcome(" "),verif);
+		assertEquals(Welcome.welcome(""),verif);
+		assertEquals(Welcome.welcome(null),verif);
+	}
+	
+	@Test
+	void test3() {
+		assertEquals(Welcome.welcome("JERRY"),"HELLO, JERRY !");
+	}
+	
+//	@Test
+//	void test4() {
+//		assertEquals(Welcome.welcome("amy,bob"),"Hello, Amy, Bob");
+//	}
 }
