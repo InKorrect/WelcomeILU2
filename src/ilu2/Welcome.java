00c83b75e17a5e3aaee44dc3bc58b4279630a10a
gabriel.plique@gmail.com <gabriel.plique@gmail.com>
2023-04-24 17:35:43 +0200

    EX_6

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -7,21 +7,39 @@ public class Welcome {
 		String reponse = null;
 		String reponse3= null;
 		String[] part;
+		String part1;
 		if(" ".equals(input)||input==null||"".equals(input)){
 			 StringBuilder str2 = new StringBuilder("Hello, my friend");
 			 reponse=str2.toString();
 			 return reponse;
 		}
 		part=input.split(",");
+		int [] nomMaj=new int [part.length];
 		if(!part[0].equals(input)) {
+			int o=0;
 			StringBuilder str4 = new StringBuilder("Hello");
 			for(int i=0;i<part.length;i++) {
-			
-			String part1=part[i].substring(0, 1).toUpperCase() + part[i].substring(1);
-			str4.append(", ");
-			str4.append(part1);
+			if(!part[i].equals(part[i].toUpperCase())) {
+				part1=part[i].substring(0, 1).toUpperCase() + part[i].substring(1);
+				str4.append(", ");
+				str4.append(part1);
+			}
+			else if(part[i].equals(part[i].toUpperCase())){
+				nomMaj[o]=i;
+				o++;
+			}
 			}
 			reponse=str4.toString();
+			if(nomMaj[0]!=0) {
+				String reponse2;
+				StringBuilder str6 = new StringBuilder(". AND HELLO, ");
+				part1=part[nomMaj[0]];
+				str6.append(part1);
+				str6.append(" !"); 
+				reponse2=str6.toString();
+				reponse=reponse+reponse2.toUpperCase()
+;			}
+			
 		}
 		else{
 			if(!input.equals(input.toUpperCase())){
